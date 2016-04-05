c2ac742ca973636f89a2644d1ccc31767e87b632
Sveta <Lotishko_sveta@mail.ru>
2016-04-05 18:11:46 +0300

    Create Controller

---
+++ src/main/java/test/controller/DeleteDepartmnetsController.java
@@ -0,0 +1,25 @@
+package test.controller;
+
+import test.dao.impl.DepartmentDAOImpl;
+import test.service.impl.DepartmentServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created on 05.04.16.
+ */
+public class DeleteDepartmnetsController implements InternalController {
+    @Override
+    public void executor(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int action = Integer.parseInt(request.getParameter("delete"));
+
+        DepartmentServiceImpl departmentService = new DepartmentServiceImpl();
+        departmentService.delete(action);
+
+        response.sendRedirect("/");
+    }
+}
