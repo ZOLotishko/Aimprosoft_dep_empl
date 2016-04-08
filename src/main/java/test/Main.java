83ec4e0d546c947ba109d34b556cf7945d7abe6c
DimaShulga <mediv.dima@gmail.com>
2016-04-08 23:34:57 +0300

    Update Main.java
    ....

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -14,7 +14,7 @@ import java.util.Date;
  * Created by user on 04.04.16.
  */
 public class Main {
-    public static void main(String[] args ) {
+    public static void main(String[] args ) {//what the fuck
 
 
     }
